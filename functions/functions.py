dcf57a5273b3d85b9c1b3e7fd8fe1eeac4465915
diazhdez <jd5531066@gmail.com>
2024-04-25 13:37:54 -0600

    minor changes

--- functions/functions.py
+++ functions/functions.py
@@ -2,6 +2,7 @@ import database.database as dbase
 
 db = dbase.dbConnection()
 
+
 def get_user(email):
     user = db['users'].find_one({'email': email})
     return user
@@ -10,6 +11,7 @@ def get_user(email):
 def get_admin(email):
     admin = db['admin'].find_one({'email': email})
     return admin
+
 
 # Función para verificar si el usuario ha completado el cuestionario
 def user_has_completed_survey(user_id):
